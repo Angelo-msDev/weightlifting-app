674b76bf7419902c9a7b760ff3a8615cc095f666
Angelo-msDev <angelo.mascarenhas.de.souza@gmail.com>
2026-04-24 09:55:05 -0300

    Update sw.js

--- frontend/sw.js
+++ frontend/sw.js
@@ -1,23 +1,30 @@
 const CACHE_NAME = 'weightlifting-v1';
+
+// Arquivos que o app vai salvar para funcionar offline
 const assets = [
   './',
   './index.html',
   './style.css',
   './script.js',
-  './manifest.json',
-  'https://cdn-icons-png.flaticon.com/512/2964/2964514.png'
+  './manifest.json'
 ];
 
-// Instalação do Service Worker e Cache dos arquivos
+// Instalação: Salva os arquivos básicos
 self.addEventListener('install', event => {
   event.waitUntil(
     caches.open(CACHE_NAME).then(cache => {
-      return cache.addAll(assets);
+      console.log('Cache aberto com sucesso');
+      return cache.addAll(assets).catch(err => console.log('Aviso: Alguns arquivos não foram cacheados', err));
     })
   );
 });
 
-// Responde com os arquivos do cache quando estiver offline
+// Ativação: Limpa caches antigos
+self.addEventListener('activate', event => {
+  console.log('Service Worker ativo!');
+});
+
+// Estratégia de Busca: Tenta o Cache primeiro, se não tiver, vai na Rede
 self.addEventListener('fetch', event => {
   event.respondWith(
     caches.match(event.request).then(response => {
